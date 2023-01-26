f2d691dc3bf4fcaf9bb76fa3e7a09b675f7d53ed
Justus Rudolph <justus.katte@gmail.com>
2023-01-26 13:28:03 +0000

    More debug messages added for cluster finding.

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -31,21 +31,23 @@
 namespace traccc::cuda {
 namespace kernels {
 
-__device__ void print_grid_info() {
-    //printf("Grid dim:   (%d, %d, %d)\n", gridDim.x, gridDim.y, gridDim.z);
-    // printf("Block idx:  (%d, %d, %d)\n", blockIdx.x, blockIdx.y, blockIdx.z);
-    //printf("Block dim:  (%d, %d, %d)\n", blockDim.x, blockDim.y, blockDim.z);
-    // printf("Thread Idx: (%d, %d, %d)\n", threadIdx.x, threadIdx.y, threadIdx.z);
-    printf("(Block, Thread) idx in x:  (%d, %d)\n", blockIdx.x, threadIdx.x);
 
-}
+__global__ void find_clusters_ind(
+    const cell_container_types::const_view cells_view,
+    vecmem::data::jagged_vector_view<unsigned int> sparse_ccl_indices_view,
+    vecmem::data::vector_view<std::size_t> clusters_per_module_view) {
+        /*
+        this function is the same as find_clusters but instead of every module
+        being a thread, every cell is a thread instead
+        */
+       unsigned int module_number = 0;  // temp
+    }
 
 __global__ void find_clusters(
     const cell_container_types::const_view cells_view,
     vecmem::data::jagged_vector_view<unsigned int> sparse_ccl_indices_view,
     vecmem::data::vector_view<std::size_t> clusters_per_module_view) {
 
-    //print_grid_info();
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     cell_container_types::const_device cells_device(cells_view);
     vecmem::jagged_device_vector<unsigned int> device_sparse_ccl_indices(
@@ -55,17 +57,24 @@ __global__ void find_clusters(
 
     bool check_all = false;
     // pick the below module because it has many activated cells
+    // 755 has the most cells, 2464 has exactly one cluster with the most cells (5)
     int module_to_check = 755;  // module number that is being probed if above false
 
     // if all modules are to be checked, then check if index is in range
     // otherwise, check only if index is right
     bool check = (check_all && (idx < cells_device.size())) || (idx == module_to_check);
-    
     if (check) {
         // first print the cells for this module
         const vecmem::device_vector<const traccc::cell>& cells =
             cells_device.at(idx).items;
+        const traccc::cell_module cells_header = cells_device.at(idx).header;
         size_t n_cells = cells.size();
+
+        // TODO figure out what "module" means here
+        printf("cells_header: id: %d, module: %d range0: (%d, %d), range1: (%d, %d)\n",
+               (int) cells_header.event, (int) cells_header.module, 
+               (int) cells_header.range0[0], (int) cells_header.range0[1],
+               (int) cells_header.range1[0], (int) cells_header.range1[1]);
 
         for (int i=0; i < n_cells; i++) {
             const traccc::cell cell = cells[i];
@@ -94,8 +103,10 @@ __global__ void find_clusters(
     device::find_clusters(threadIdx.x + blockIdx.x * blockDim.x, cells_view,
                           sparse_ccl_indices_view, clusters_per_module_view);
 
+    //printf("Finished, %d\n", (int) check);
     if (check) {
         unsigned int n_sparse_ccl_indices = device_sparse_ccl_indices.at(idx).size();
+        //printf("n_sparse_ccl_indices: %u\n", n_sparse_ccl_indices);
         // print outputs from clusterisation algo
         for (int i=0; i < n_sparse_ccl_indices; i++) {
             unsigned int cell_cluster_number = device_sparse_ccl_indices.at(idx).at(i);
@@ -103,6 +114,11 @@ __global__ void find_clusters(
                 idx, (int) device_clusters_per_module.at(idx), i, cell_cluster_number);
         }
     }
+    // if (device_clusters_per_module.at(idx) == 2) {
+    //     printf("Module with two clusters: %d. Number of cells: %d\n",
+    //            idx, cells_device.at(idx).items.size());
+    // }
+    //printf("Module %d has %d clusters\n.", idx, device_clusters_per_module.at(idx));
 }
 
 __global__ void count_cluster_cells(
@@ -181,6 +197,17 @@ clusterization_algorithm::output_type clusterization_algorithm::operator()(
     // Get the view of the cells container
     auto cells_data =
         get_data(cells_per_event, (m_mr.host ? m_mr.host : &(m_mr.main)));
+
+    // auto *headers = &cells_data.headers;
+    // auto *cells = vecmem::get_data(&cells_data.items);
+    // for (unsigned int j = 0; j < num_modules; j++){
+    //     auto module_cells = cells[j];
+    //     std::sort(module_cells->begin(), module_cells->end(),
+    //         [](auto a, auto b)
+    //             {
+    //                 return a.channel1 > b.channel1;
+    //             });
+    // }
 
     // Get the sizes of the cells in each module
     auto cell_sizes = copy.get_sizes(cells_data.items);
